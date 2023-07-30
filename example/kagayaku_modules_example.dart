5e733df6eae5429c924fa6a2f4d98ee161015aba
Zerolmntrix <zeryon.lmntrix@gmail.com>
2023-07-30 15:17:26 +0100

    initial commit

---
+++ example/kagayaku_modules_example.dart
@@ -0,0 +1,6 @@
+import 'package:kagayaku_modules/kagayaku_modules.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/kagayaku_modules_test.dart
@@ -0,0 +1,16 @@
+import 'package:kagayaku_modules/kagayaku_modules.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
